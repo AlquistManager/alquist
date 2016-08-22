af6a662e6fd8e892eec9c9c70f9bda743fbf73a4
ermrk <ermrk@seznam.cz>
2016-08-22 11:44:45 +0200

    Input takes and returns right values, call to state execute missing

--- IO/input.py
+++ IO/input.py
@@ -9,43 +9,16 @@ flask = Flask(__name__)
 cors = CORS(flask)
 
 
-# Used for registration of new users
-@flask.route("/start", methods=['POST'])
-def create_new_session():
-    session_id = sessions.register_user()
-    return jsonify(session_id=session_id)
-
-
 # Used for sending messages to the bot
 @flask.route("/", methods=['POST'])
 def get_input():
     try:
-        session_id = request.json['session_id']
-        if session_id in sessions.sessions:
-            # user with session_id has been created already
-            session = sessions.sessions[session_id]
-            text = request.json['text']
-            session.input_buffer.put(text)
-            return jsonify(ok=True)
-        else:
-            # wrong user id
-            return jsonify(ok=False, message="User with given id doesn't exist.")
-    except KeyError:
-        # Missing 'session_id' or 'text'
-        return jsonify(ok=False, message="Missing parameters.")
-
-
-@flask.route("/session", methods=['POST'])
-def get_session_history():
-    try:
-        session_id = request.json['session_id']
-        if session_id in sessions.sessions:
-            # user with session_id has been created already
-            session_history = sessions.sessions[session_id].session_history
-            return jsonify(ok=True, session_history=session_history)
-        else:
-            # wrong user id
-            return jsonify(ok=False, message="User with given id doesn't exist.")
+        state_name = request.json['state_name']
+        context = request.json['context']
+        text = request.json['text']
+        #TODO call state execute
+        #TODO make some response
+        return jsonify(text=None,state_name=None,context=None)
     except KeyError:
         # Missing 'session_id' or 'text'
         return jsonify(ok=False, message="Missing parameters.")
