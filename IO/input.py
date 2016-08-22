cb3d242e3f692161d1faa39389c75d029b40f85c
ermrk <ermrk@seznam.cz>
2016-08-22 12:52:02 +0200

    Creating session id

--- IO/input.py
+++ IO/input.py
@@ -1,4 +1,5 @@
 # Handles input from the user by Flask
+import uuid
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -16,9 +17,14 @@ def get_input():
         text = request.json['text']
         state = request.json['state']
         context = request.json['context']
+        session = request.json['session']
+
+        # create UUID for session if it does't exist
+        if session is "":
+            session = str(uuid.uuid4())
         # TODO call state execute
         # TODO make some response
-        return jsonify(text=text, state=state, context=context)
+        return jsonify(text=text, state=state, context=context, session=session)
     except KeyError:
         # Missing 'session_id' or 'text'
         return jsonify(ok=False, message="Missing parameters.")
