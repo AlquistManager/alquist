49c451c597ece0c0f20a67f7854c8cf3fab98cd5
ermrk <ermrk@seznam.cz>
2016-08-22 11:50:57 +0200

    Changed names of input parameters

--- IO/input.py
+++ IO/input.py
@@ -13,9 +13,9 @@ cors = CORS(flask)
 @flask.route("/", methods=['POST'])
 def get_input():
     try:
-        state_name = request.json['state_name']
-        context = request.json['context']
         text = request.json['text']
+        state = request.json['state']
+        context = request.json['context']
         #TODO call state execute
         #TODO make some response
         return jsonify(text=None,state_name=None,context=None)
