f406f769e3acbd0a973b01f9eb090fab813a9ce6
Jakub Konrád <konrajak@fel.cvut.cz>
2016-08-23 15:05:37 +0200

    Fix bug: Alquist now properly sends all messages in sequence

--- solver.py
+++ solver.py
@@ -12,18 +12,15 @@ def process_request(state_name, context, text, session):
         request_data = current_state.execute(request_data)
         has_next = request_data.get('next_state', False)
         if has_next:
-            next_type = str(state_dict['states'][has_next].get('type', lambda: "nothing"))
-            if next_type == str('<class \'states.user_input.InputUser\'>'):
-                if request_data.get('response', '') != '':
-                    main_logger.info("BOT SAYS: " + request_data['response'], extra={'uid': session})
+            if request_data.get('response', False):
+                main_logger.info("BOT SAYS: " + request_data['response'], extra={'uid': session})
                 return request_data
             state_name = has_next
         else:
             request_data.update({'next_state': 'END'})
-            if request_data.get('response', '') != '':
+            if request_data.get('response', ''):
                 main_logger.info("BOT SAYS: " + request_data['response'], extra={'uid': session})
             main_logger.info("===== SESSION END =====", extra={'uid': session})
-
             return request_data
 
 
--- tests/test_dialogue/run.py
+++ tests/test_dialogue/run.py
@@ -1,5 +1,6 @@
 from yaml_parser.yaml_parser import YamlParser
 import solver
+import loaded_states
 
 YamlParser()
 request_data = {"context": {}, "state_name": "init", "text": ''}
@@ -7,6 +8,6 @@ while request_data.get('state_name')!="END":
     request_data = solver.process_request(request_data.get('state_name'), request_data.get('context'), request_data.get('text'), "TEST")
     request_data['state_name'] = request_data['next_state']
     print(request_data)
-    if request_data.get('state_name')!="END":
+    if request_data.get('next_state') != 'END' and str(loaded_states.state_dict['states'][request_data.get('next_state')]['type'])==str('<class \'states.user_input.InputUser\'>'):
         request_data['text'] = input()
 
