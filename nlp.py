233b43c161b183210cbfbf090341847b03d65e42
Jakub Konrád <konrajak@fel.cvut.cz>
2016-08-30 16:42:40 +0200

    Remove confidence threshold for alquist

--- nlp.py
+++ nlp.py
@@ -12,12 +12,12 @@ def get_entities(text):
     resp = client.message(text)
     ent_out = {}
     for key in resp['entities']:
-        if resp['entities'][key][0]['confidence'] > 0.6:
+        if resp['entities'][key][0]['confidence'] > 0:
             ent_out.update({key: resp['entities'][key][0]['value']})
 
     intent = resp.get('intent', False)
     if intent:
-        if intent[0]['confidence'] > 0.6:
+        if intent[0]['confidence'] > 0:
             ent_out.update({'intent': intent[0]['value']})
     return ent_out
 
