d333fd58b0371ecf9e5235b5b415b5e8dfbd82eb
ermrk <ermrk@seznam.cz>
2016-09-29 12:51:34 +0200

    Logging to google doc

---
+++ dialogue_logger.py
@@ -0,0 +1,34 @@
+import threading
+
+import requests
+import urllib.parse
+
+baseURL = "https://docs.google.com/forms/d/e/1FAIpQLSeWIBYU_Cv3Bbg3RMXyUdjE-aK3DW3yeEm443wl1JVnFGHb8A/formResponse?"
+sessionIDField = "entry.109028851"
+logField = "entry.284069394"
+threadLock = threading.Lock()
+
+
+def log(log, sessionID):
+    encoded_session_id = urllib.parse.quote(sessionID)
+    encoded_log = urllib.parse.quote(log)
+    request = baseURL + sessionIDField + "=" + encoded_session_id + "&" + logField + "=" + encoded_log + "&submit=Submit"
+    log_thread = logThread(request)
+    log_thread.start()
+
+
+def send_log(request):
+    requests.get(request)
+
+
+class logThread(threading.Thread):
+    def __init__(self, request):
+        self.request = request
+        threading.Thread.__init__(self)
+
+    def run(self):
+        # Get lock to synchronize threads
+        threadLock.acquire()
+        send_log(self.request)
+        # Free lock to release next thread
+        threadLock.release()
--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 import copy
+import dialogue_logger
 
 from loaded_states import state_dict
 from logger import main_logger
@@ -7,17 +8,22 @@ from logger import main_logger
 def process_request(state_name, context, text, session):
     if text == '!undo':
         prev_context = context.get('previous_context', {})
-        request_data = {'context': prev_context, 'text': prev_context.get('previous_text', ''), 'session': session, 'next_state': context.get('previous_request', 'init')}
+        request_data = {'context': prev_context, 'text': prev_context.get('previous_text', ''), 'session': session,
+                        'next_state': context.get('previous_request', 'init')}
         state_name = prev_context.get('previous_request', 'init')
         main_logger.info("UNDO", extra={'uid': session})
+        dialogue_logger.log("UNDO", session)
         main_logger.info("GOTO: " + str(request_data['next_state']), extra={'uid': session})
+        dialogue_logger.log("GOTO: " + str(request_data['next_state']), session)
     else:
         request_data = {'context': context, 'text': text, 'session': session}
     context.update({'previous_context': copy.deepcopy(context), 'previous_request': state_name, 'previous_text': text})
     if text != '':
-            main_logger.info("USER SAYS: "+request_data['text'], extra={'uid': session})
+        main_logger.info("USER SAYS: " + request_data['text'], extra={'uid': session})
+        dialogue_logger.log("USER SAYS: " + request_data['text'], session)
     while True:
-        main_logger.debug("Entering State: "+state_name, extra={'uid': session})
+        main_logger.debug("Entering State: " + state_name, extra={'uid': session})
+        dialogue_logger.log("Entering State: " + state_name, session)
         current_state = build_state(state_name)
         request_data = current_state.execute(request_data)
         has_next = request_data.get('next_state', False)
@@ -26,13 +32,16 @@ def process_request(state_name, context, text, session):
             if next_type == str('<class \'states.user_input.InputUser\'>'):
                 if request_data.get('response', '') != '':
                     main_logger.info("BOT SAYS: " + str(request_data['response']), extra={'uid': session})
+                    dialogue_logger.log("BOT SAYS: " + str(request_data['response']), session)
                 return request_data
             state_name = has_next
         else:
             request_data.update({'next_state': 'END'})
             if request_data.get('response', '') != '':
                 main_logger.info("BOT SAYS: " + str(request_data['response']), extra={'uid': session})
+                dialogue_logger.log("BOT SAYS: " + str(request_data['response']), session)
             main_logger.info("===== SESSION END =====", extra={'uid': session})
+            dialogue_logger.log("===== SESSION END =====", session)
 
             return request_data
 
