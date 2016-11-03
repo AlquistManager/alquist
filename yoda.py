f74dfcde388ff6b17d934458f3a490621d1352c3
Nguyen Hoang Long <roman.long@email.cz>
2016-11-03 16:43:08 +0100

    fixed yoda

--- yoda.py
+++ yoda.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import requests
+import time
 url = "http://cloud.ailao.eu:4578/q"
 
 def process_question(text):
-    yoda_qid = requests.post(URL, data={'text': text}).json()['id']
+    yoda_qid = requests.post(url, data={'text': text}).json()['id']
     while True:
         time.sleep(0.5)
-        data = requests.get(URL + '/' + yoda_qid).json()
+        data = requests.get(url + '/' + yoda_qid).json()
         if data["finished"]:
             break
     return data['answers'][0]['text']
